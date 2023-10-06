513504c5f15e64e9a7447425d0b8d0fb9e7675d4
Andrew Reynolds <areynold@illinois.edu>
2023-10-06 12:36:00 -0500

    stub conversion to do list

--- src/transcript-formatter.ts
+++ src/transcript-formatter.ts
@@ -1,5 +1,20 @@
 export default class TranscriptFormatter {
     public format(path: string): void {
-        throw new Error("not implemented");
+        console.debug('read file as json');
+        const data: JSON = JSON.parse('');
+
+        console.debug('check if speaker labels present');
+        if ('speaker_labels' in data) {
+            console.debug('get speaker labels');
+
+            console.debug('foreach speaker label get segments');
+            console.debug('foreach segment get items');
+            console.debug('foreach item get start_time, label speaker')
+            console.debug('foreach item get start time, get content or punctuation')
+            console.debug('sort lines')
+        } else {
+            console.debug('get raw transcript');
+        }
+
     }
 }
