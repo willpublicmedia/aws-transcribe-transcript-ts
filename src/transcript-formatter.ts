4d9129733af72c816d3092371880e36e51ee6a58
Andrew Reynolds <areynold@illinois.edu>
2023-10-09 12:31:22 -0500

    label speakers

--- src/transcript-formatter.ts
+++ src/transcript-formatter.ts
@@ -19,36 +19,58 @@ export default class TranscriptFormatter {
 
             console.debug('foreach segment get items');
             const items = results.items;
-            const lines = [];
+            const lines: { speaker: string, line: string, time: string }[] = [];
             let line = ''
-            let time = 0
-            let speaker = 'null'
+            let time = '0'
+            let speaker: string | null = null
             let i = 0
 
             items.forEach(item => {
                 i = i + 1;
+
+                let current_speaker: string | null = null;
                 const content = item.alternatives[0].content;
 
                 console.debug('foreach item get start_time, label speaker')
+                if ('start_time' in item) {
+                    current_speaker = speaker_start_times.find(s => s.start_time == item.start_time)?.label ?? '';
+                }
+
+                console.debug('foreach item get start time, get content or punctuation')
+                if (item.type === 'punctuation') {
+                    line = line + content;
+                }
+
+                if (current_speaker != speaker) {
+                    if (speaker) {
+                        lines.push({ speaker: speaker, line: line, time: time });
+                    }
+
+                    line = content;
+                    speaker = current_speaker;
+                    time = item.start_time;
+                } else if (item.type !== 'punctuation') {
+                    line = line + ' ' + content;
+                }
             });
-            // for item in items:
-            // 	i=i+1
-            // 	content = item['alternatives'][0]['content']
-            // 	if item.get('start_time'):
-            // 		current_speaker=speaker_start_times[item['start_time']]
-            // 	elif item['type'] == 'punctuation':
-            // 		line = line+content
-            // 	if current_speaker != speaker:
-            // 		if speaker:
-            // 			lines.append({'speaker':speaker, 'line':line, 'time':time})
-            // 		line=content
-            // 		speaker=current_speaker
-            // 		time=item['start_time']
-            // 	elif item['type'] != 'punctuation':
-            // 		line = line + ' ' + content
-            
-            console.debug('foreach item get start time, get content or punctuation')
+
+            lines.push({ speaker: speaker ?? '', line: line, time: time });
+
             console.debug('sort lines')
+            const sorted_lines = lines.sort(l => Date.parse(l.time))
+            sorted_lines.forEach(data => {
+                let line = '[' + data.time + '] ';
+
+                if ('speaker' in data) {
+                    line = line + data.speaker + ' ';
+                }
+
+                if ('line' in data) {
+                    line = line + data.line;
+                }
+
+                console.debug(line);
+            });
         } else {
             console.debug('get raw transcript');
         }
