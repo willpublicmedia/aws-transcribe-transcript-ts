1e5814d3cedbd515c8be1876e9b9b5ee6a5328d1
Andrew Reynolds <areynold@illinois.edu>
2023-10-09 12:09:57 -0500

    map speaker start times

--- src/transcript-formatter.ts
+++ src/transcript-formatter.ts
@@ -2,9 +2,19 @@ import { TranscribeJobOutput } from "./types/transcribe-job-output";
 
 export default class TranscriptFormatter {
     public format(data: TranscribeJobOutput): void {
+        const results = data.results[0];
+
         console.debug('check if speaker labels present');
-        if ('speaker_labels' in data) {
+        if ('speaker_labels' in results) {
             console.debug('get speaker labels');
+            const labels = results.speaker_labels?.segments;
+            const speaker_start_times: { start_time: string, label: string }[] = [];
+
+            labels?.forEach(label => {
+                label.items.forEach(item => {
+                    speaker_start_times.push({ start_time: item.start_time, label: item.speaker_label });
+                });
+            });
 
             console.debug('foreach speaker label get segments');
             console.debug('foreach segment get items');
