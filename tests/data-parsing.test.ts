b1ba57cce0db7017fad7e4269abd71ea831a8451
Andrew Reynolds <areynold@illinois.edu>
2023-10-23 11:45:45 -0500

    correct async loop structure

--- tests/data-parsing.test.ts
+++ tests/data-parsing.test.ts
@@ -3,21 +3,24 @@ import * as fsPromises from 'fs/promises';
 import { TranscribeJobOutput, TranscribeJobResult } from '../src/types/transcribe-job-output';
 import DataBuilder from './data-builder';
 
-test('parsed types are correct', () => {
+test('parsed types are correct', async () => {
     const builder = new DataBuilder();
     const items = builder.GenerateTestData();
-    items.forEach(async item => {
-        const data = await fsPromises.readFile(item.file, { encoding: 'utf8' });
-        const parsed: TranscribeJobOutput = JSON.parse(data);
-        expect(parsed).toHaveProperty('jobName');
-        expect(parsed).toHaveProperty('accountId');
-        expect(parsed).toHaveProperty('results');
-        expect(parsed).toHaveProperty('status');
 
-        const results: TranscribeJobResult = parsed.results;
-        expect(results).toHaveProperty('transcripts');
-        expect(results).toHaveProperty('items');
-    });
+    await Promise.all(
+        items.map(async item => {
+            const data = await fsPromises.readFile(item.file, { encoding: 'utf8' });
+            const parsed: TranscribeJobOutput = JSON.parse(data);
+            expect(parsed).toHaveProperty('jobName');
+            expect(parsed).toHaveProperty('accountId');
+            expect(parsed).toHaveProperty('results');
+            expect(parsed).toHaveProperty('status');
+
+            const results: TranscribeJobResult = parsed.results;
+            expect(results).toHaveProperty('transcripts');
+            expect(results).toHaveProperty('items');
+        })
+    );
 });
 
 test('account ID has been anonymized', async () => {
