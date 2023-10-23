9981770b6000a83b3c144ca473b7ff67b6350dbf
Andrew Reynolds <areynold@illinois.edu>
2023-10-23 11:44:42 -0500

    check aws account ID

--- tests/data-parsing.test.ts
+++ tests/data-parsing.test.ts
@@ -19,3 +19,17 @@ test('parsed types are correct', () => {
         expect(results).toHaveProperty('items');
     });
 });
+
+test('account ID has been anonymized', async () => {
+    const builder = new DataBuilder();
+    const items = builder.GenerateTestData();
+    await Promise.all(
+        items.map(async item => {
+            const expected = '111111111111';
+            const data = await fsPromises.readFile(item.file, { encoding: 'utf-8' });
+            const parsed: TranscribeJobOutput = JSON.parse(data);
+            const actual = parsed.accountId;
+            expect(actual).toBe(expected);
+        })
+    );
+});
